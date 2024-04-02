9e1f724170d929be72d2e61b69669bb1f240d585
Kai Yu <kai.yu@nih.gov>
2024-04-02 15:49:50 -0400

    Updated sample sheet format and sample sheet loading function

--- scripts/load.py
+++ scripts/load.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
-import sys
+import os,sys
 import pandas as pd
 
 def samplesheet(sspath):
-    df = pd.read_csv(sspath, sep='\t', index_col=2)
+    '''
+    Input:
+    Sample sheet with following columns:
+    SAMPLE	PATIENT	GENDER  EVENT	SID	CRAM	MD5
+    
+    Output:
+    sample: dict=[SAMPLE:{PATIENT:***, GENDER:M/F/U, EVENT:TOD/CR, SID:***, CRAM:***, MD5:***}]
+    '''
+    df = pd.read_csv(sspath, sep='\t', index_col=0)
+    patient = {}
     sample = {}
-    lib = {}
-    run = {}
-    for i in df.index:
-        ss = df.loc[i,'Sample']
-        li = df.loc[i,'Lib']
-        if ss not in sample:
-            sample[ss] = []
-        if li not in lib:
-            lib[li] = []
-        if li not in sample[ss]:
-            sample[ss].append(li)
-        if i not in lib[li]:
-            lib[li].append(i)
-        run[i] = {n:df.loc[i,n] for n in df.columns}
-    return sample,lib,run
+    tasks = {'germline':[],'somatic_ss':[], 'somatic_paired':[]}
+    for s in df.index:
+        tasks['germline'].append(s)
+        p = df.loc[s,'PATIENT']
+        e = df.loc[s,'EVENT']
+        if p not in patient:
+            patient[p] = {}
+        sample[s] = df.loc[s].to_dict()
+        patient[p][e] = s
+    for p in patient:
+        if 'TOD' in patient[p]:
+            if 'CR' in patient[p]:
+                tasks['somatic_paired'].append([patient[p]['TOD'], patient[p]['CR']])
+            else:
+                tasks['somatic_ss'].append(patient[p]['TOD'])
+    return sample, patient, tasks
 
+def test_samplesheet():
+    sspath='%s/../sample_sheet.txt'%(os.path.dirname(os.path.abspath(__file__)))
+    print(samplesheet(sspath))
+    
 if __name__ == '__main__':
-    print(samplesheet(sys.argv[1]))
+    test_samplesheet()
