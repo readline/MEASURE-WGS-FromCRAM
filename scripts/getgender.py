7c5a18d654c5e04ac42746464053308d6fe1fe4e
Kai Yu <funsinaime@gmail.com>
2024-08-08 16:25:12 -0400

    Added getgender script for CNVkit usage

---
+++ scripts/getgender.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os,sys
+
+def main():
+    try:
+        xcov = sys.argv[1]
+    except:
+        sys.exit(sys.argv[0]+' [mosdepth.summary.txt file]')
+    data = {}
+    with open(xcov) as infile:
+        while 1:
+            line = infile.readline()
+            if not line:
+                break
+            c = line.strip().split('\t')
+            data[c[0]] = c[3]
+    if float(data['chrY']) / float(data['total']) > 0.25:
+        return '-y'
+    else:
+        return ''
+if __name__ == '__main__':
+    print(main())
