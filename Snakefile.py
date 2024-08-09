02aa5b99496cc28bc4f992ebeda677ba2b0fcdc7
Kai Yu <funsinaime@gmail.com>
2024-08-08 21:22:30 -0400

    .

--- Snakefile.py
+++ Snakefile.py
@@ -138,8 +138,11 @@ rule all:
         #     join(config['workdir'], "46.somatic_ss_cnv__cnvkit", "{sample}", "cnvkit.ok"),
         #     sample=tasks['somatic_ss']+[i[0] for i in tasks['somatic_paired']],
         # ),
+        # expand(
+        #     join(config['workdir'], "47.somatic_ss_cnv__purple", "{sample}", "M00001_TOD_BM.purple.sv.vcf.gz"),
+        #     sample=tasks['somatic_ss']+[i[0] for i in tasks['somatic_paired']],
+        # ),
         expand(
-            # join(config['workdir'], "47.somatic_ss_cnv__purple", "{sample}", "M00001_TOD_BM.purple.sv.vcf.gz"),
-            join(config['workdir'], "47.somatic_ss_cnv__purple", "{sample}", "amber", "amber.ok"),
+            join(config['workdir'], "48.somatic_ss_cnv__canvas", "{sample}", "CNV.vcf.gz"),
             sample=tasks['somatic_ss']+[i[0] for i in tasks['somatic_paired']],
         ),
